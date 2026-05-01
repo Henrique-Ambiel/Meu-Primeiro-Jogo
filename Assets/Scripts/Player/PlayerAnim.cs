3b2f4bfde35a18107226db58f085614d5c7636f0
Henrique-Ambiel <146786119+Henrique-Ambiel@users.noreply.github.com>
2026-05-01 09:16:38 -0300

    Configurando animação de pulo

--- Assets/Scripts/Player/PlayerAnim.cs
+++ Assets/Scripts/Player/PlayerAnim.cs
@@ -3,10 +3,15 @@ using UnityEngine;
 public class PlayerAnim : MonoBehaviour
 {
     private Animator animator;
+    private IsGroundedCheck groundedCheck;
 
     private void Awake()
     {
         animator = GetComponent<Animator>();
+        groundedCheck = GetComponent<IsGroundedCheck>();
+
+        Debug.Log("Animator: " + animator);
+        Debug.Log("GroundedCheck: " + groundedCheck);
     }
 
 
@@ -15,9 +20,14 @@ public class PlayerAnim : MonoBehaviour
         float movement = GameManager.instance.inputManager.Movement;
 
         bool isWalking = Mathf.Abs(movement) > 0.01f; //Verifica se o player andou
+        bool isJumping = !groundedCheck.IsGrounded();
 
         Debug.Log("isWalking: " + isWalking + " | movement: " + movement); //Testando se o personagem está andando
 
         animator.SetBool("isWalk", isWalking); //Inicia animação
+        animator.SetBool("isJump", isJumping); //Inicia animação de pulo
+
+        Debug.Log("GameManager: " + GameManager.instance); //Verifica se está instanciando o GameManager
+        Debug.Log("InputManager: " + GameManager.instance?.inputManager); //Verifica se está instanciando o InputManager
     }
 }
